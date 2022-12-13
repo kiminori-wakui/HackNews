17c5cb1bf16399a984130f89f127c150e8f850c1
kiminori.wakui <kiminori.wakui@gmail.com>
2022-12-13 20:46:26 +0900

    Create Project

---
+++ app/src/main/java/com/example/hacknews/ui/JetnewsNavGraph.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.hacknews.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.hacknews.data.AppContainer
+import com.example.hacknews.ui.home.HomeRoute
+import com.example.hacknews.ui.home.HomeViewModel
+import com.example.hacknews.ui.interests.InterestsRoute
+import com.example.hacknews.ui.interests.InterestsViewModel
+
+@Composable
+fun HacknewsNavGraph(
+    appContainer: AppContainer,
+    isExpandedScreen: Boolean,
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    openDrawer: () -> Unit = {},
+    startDestination: String = HacknewsDestinations.HOME_ROUTE
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(HacknewsDestinations.HOME_ROUTE) {
+            val homeViewModel: HomeViewModel = viewModel(
+                factory = HomeViewModel.provideFactory(appContainer.postsRepository)
+            )
+            HomeRoute(
+                homeViewModel = homeViewModel,
+                isExpandedScreen = isExpandedScreen,
+                openDrawer = openDrawer
+            )
+        }
+        composable(HacknewsDestinations.INTERESTS_ROUTE) {
+            val interestsViewModel: InterestsViewModel = viewModel(
+                factory = InterestsViewModel.provideFactory(appContainer.interestsRepository)
+            )
+            InterestsRoute(
+                interestsViewModel = interestsViewModel,
+                isExpandedScreen = isExpandedScreen,
+                openDrawer = openDrawer
+            )
+        }
+    }
+}
